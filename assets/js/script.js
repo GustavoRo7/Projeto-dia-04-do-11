6ca950efa8061973127d64722f1263ce081b2487
Your Name <you@example.com>
2024-11-06 15:59:46 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            alert('Detalhes do jogo ainda não disponíveis!');
+        });
+    });
+});
